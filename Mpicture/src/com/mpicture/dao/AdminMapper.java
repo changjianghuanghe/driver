8edbfb7466c87345f3e632ccc9fd7a50d99dae51
xingbake6113 <917113374@qq.com>
2017-09-20 11:58:38 +0800

    ~~

--- Mpicture/src/com/mpicture/dao/AdminMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mpicture.dao;
-
-import com.mpicture.entity.Admin;
-
-import tk.mybatis.mapper.common.Mapper;
-
-public interface AdminMapper extends Mapper<Admin>{
-
-}
--- Mpicture/src/com/mpicture/dao/ProductMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mpicture.dao;
-
-import com.mpicture.entity.Product;
-
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ProductMapper extends Mapper<Product>{
-
-}
